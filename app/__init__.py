dbdb61ae3365108047ca5260fca654bde3926025
r00tus4r <diyorbek.qodirboyev05@gmail.com>
2025-07-22 20:32:13 +0500

    feat: add custom error pages for 403, 404, and 500 errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
@@ -26,6 +26,18 @@ def create_app():
     login_manager.login_message = 'Ushbu sahifani ko‘rish uchun tizimga kiring!'
     login_manager.login_message_category = 'info'
     login_manager.login_view = 'auth.login'
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('403.html'), 403
 
     from .models import User
 
